9133c0795436a3e6bcfca09f2a03fad4bf25ad6b
Ariff-progweb <76066700+Ariff-progweb@users.noreply.github.com>
2020-12-15 14:10:09 -0600

    Create Ctrl.js

---
+++ js/Ctrl.js
@@ -0,0 +1,10 @@
+export class Ctrl {
+  /** @param {string} nombre */
+  calculaSaludo(nombre) {
+    const nombreSinEspacios = nombre ? nombre.trim() : "";
+    if (!nombreSinEspacios) {
+      throw new Error("Falta proporcionar el nombre.");
+    }
+    return `${nombreSinEspacios}.`;
+  }
+}

9d593091ba4f24ea19fe27c2a470d140be5ef469
Ariff-progweb <76066700+Ariff-progweb@users.noreply.github.com>
2020-12-15 15:05:51 -0600

    Update Ctrl.js

--- js/Ctrl.js
+++ js/Ctrl.js
@@ -1,33 +1,33 @@
 export class Ctrl {
-  Enviar(nombre) {
+  Enviarn(nombre) {
     const nombreSinEspacios = nombre ? nombre.trim() : "";
     if (!nombreSinEspacios) {
       throw new Error("Falta proporcionar el nombre.");
     }
     return `${nombreSinEspacios}.`;
   }
-    Enviar(boleta) {
+    Enviarb(boleta) {
     const boletaSinEspacios = boleta ? boleta.trim() : "";
     if (!boletaSinEspacios) {
       throw new Error("Falta proporcionar la boleta.");
     }
     return `${boletaSinEspacios}.`;
   }
-    Enviar(grupo) {
+    Enviarg(grupo) {
     const grupoSinEspacios = grupo ? grupo.trim() : "";
     if (!grupoSinEspacios) {
       throw new Error("Falta proporcionar el grupo.");
     }
     return `${grupoSinEspacios}.`;
   }
-    Enviar(materia) {
+    Enviarm(materia) {
     const materiaSinEspacios = materia ? materia.trim() : "";
     if (!materiaSinEspacios) {
       throw new Error("Falta proporcionar la materia.");
     }
     return `${materiaSinEspacios}.`;
   }
-    Enviar(fecha) {
+    Enviarf(fecha) {
     const fechaSinEspacios = fecha ? fecha.trim() : "";
     if (!fechaSinEspacios) {
       throw new Error("Falta proporcionar la fecha.");
